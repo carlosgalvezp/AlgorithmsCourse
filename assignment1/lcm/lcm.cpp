0ef69f95d71abd37e2c839ca1925dac2a818c25f
Carlos Galvez <carlosgalvezp@gmail.com>
2016-06-26 13:07:52 +0200

    Solve problem 4 from assignment 1

---
+++ assignment1/lcm/lcm.cpp
@@ -0,0 +1,38 @@
+#include <iostream>  // std::cin
+#include <cstddef>  // std::size_t
+
+std::size_t lcm(const std::size_t a, const std::size_t b);
+std::size_t gcd(const std::size_t a, const std::size_t b);
+
+int main(int argc, char** argv)
+{
+    // Get inputs
+    std::size_t a, b;
+    std::cin >> a;
+    std::cin >> b;
+
+    // Compute output
+    const std::size_t output = lcm(a, b);
+
+    // Printout
+    std::cout << output;
+    return 0;
+}
+
+std::size_t lcm(const std::size_t a, const std::size_t b)
+{
+    return (a * b) / gcd(a, b);
+}
+
+std::size_t gcd(const std::size_t a, const std::size_t b)
+{
+    if (b == 0U)
+    {
+        return a;
+    }
+    else
+    {
+        std::size_t a_prime = a%b;
+        return gcd(b, a_prime);
+    }
+}
